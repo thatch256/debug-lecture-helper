618301e05e9338c0def7347db7b41e77739009cc
thatch256 <thatch256@gmail.com>
2019-06-25 12:04:34 -0600

    Finished branch 4

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ class App extends Component {
 
   calculate() {
     let { firstNum, secondNum } = this.state;
-    let sum = firstNum + secondNum;
+    let sum = +firstNum + +secondNum;
     this.setState({
       result: sum,
       firstNum: '',
       secondNum: '',
       cost: '',
-      quatity: '',
+      quantity: '',
       taxRate: ''
     })
   }
@@ -27,7 +27,7 @@ class App extends Component {
   calcTotal() {
     let { cost, quantity, taxRate } = this.state;
     let subTotal = cost * quantity;
-    let tax = subTotal * taxRate;
+    let tax = subTotal * taxRate / 100;
     this.setState({
       total: tax + subTotal
     })
@@ -48,7 +48,7 @@ class App extends Component {
           type="number"
           onChange={(e) => this.setState({ secondNum: e.target.value })} />
         <br /><br />
-        <button onClick={() => this.calculate}>Calculate</button>
+        <button onClick={() => this.calculate()}>Calculate</button>
         {
           this.state.result ? (
             <p>Result is {this.state.result}</p>
